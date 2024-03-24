a1678a90e079909cd29d420419dee1995ff929d1
Carlos Robert <crobertdmelo@gmail.com>
2024-03-24 20:02:34 -0300

    Add show action on BlogPostsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/blog_posts/:id", to:"blog_posts#show", as: :blog_post
+
+  # post 
+  # put 
+  # patch 
+  # delete
+
   root "blog_posts#index"
 end
